971ca80a4657e6afe4afc0fa6b4c23906f3079d5
VIJAYAKRISHNANJ <vijayakrishnan2727@gmail.com>
2026-07-06 09:24:28 +0530

    Initial commit

---
+++ Geeks For Geeks/Array/Basic/Array Search.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    static int search(int[] arr, int target) {
+
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == target) {
+                return i;  
+            }
+        }
+
+        return -1;
+    }
+}
